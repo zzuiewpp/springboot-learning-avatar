5d4518c2f5639f3c9e6969a0e42fb889ec0cefa4
walker <zzuiewpp@163.com>
2018-10-13 18:12:38 +0800

    update eureka

--- springboot-eureka-client/src/main/java/com/walker/service/ProducerService.java
+++ springboot-eureka-client/src/main/java/com/walker/service/ProducerService.java
@@ -2,9 +2,9 @@ package com.walker.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,9 +18,12 @@ public class ProducerService {
 
     private static final Logger logger = LoggerFactory.getLogger(ProducerService.class);
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/getUserName")
     public String getUserName(String name) {
-        logger.info("producer provide method, param name:" + name);
+        logger.info("producer provide method, param name:" + name + "----port:" + port);
         return name;
     }
 }
